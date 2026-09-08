9bc7d5d3aeff9ad553822c75a600f673c4f4b196
Shubham Kumar <shubhamsingh72026@gmail.com>
2026-09-08 18:14:26 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,25 @@
+package com.railway.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/railway_db";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root";
+
+    private DBConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD);
+    }
+}
